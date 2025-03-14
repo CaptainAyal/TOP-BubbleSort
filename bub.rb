8875e78f9c8d620d2a979109bceb51b8feab005b
CaptainAyal <ayalmesher@gmail.com>
2025-03-14 18:37:37 +0200

    Code Complete

--- bub.rb
+++ bub.rb
@@ -1,15 +1,19 @@
 def bubble_sort (array)
-  
+  check_box = false
   i = 0
   while i < (array.length - 1) do
     if array[i] > array[i + 1]
        hold = array[i]
        array[i] = array[i + 1]
        array[i + 1] = hold
+       check_box = true
     end
     i += 1
   end
   p array
+  if check_box == true
+    bubble_sort(array)
+  end
 end
 
 
